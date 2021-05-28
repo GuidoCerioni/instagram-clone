8d6b3485ca74bf61daef1bef56b12981e2440b36
GuidoCerioni <guidocerioni98@gmail.com>
2021-05-28 18:21:27 -0300

    sync

---
+++ src/hooks/use-user.js
@@ -0,0 +1,9 @@
+// get info from firebase
+import { useState, useEffect, useContext } from 'react';
+import UserContext from '../context/user';
+
+export default function useUser() {
+  const [activeUser, setActiveUser] = useState({});
+    const { user } = useContext(UserContext);
+    
+}
