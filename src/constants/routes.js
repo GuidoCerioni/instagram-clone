040afdfe1040871a4268d8aec057faa98bb2c1e9
GuidoCerioni <guidocerioni98@gmail.com>
2021-07-13 10:04:56 -0300

    routes edit

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,5 @@
 export const DASHBOARD = '/';
-export const SIGN_UP = '/signup';
+export const SIGNUP = '/signup';
 export const PROFILE = '/p/:username';
-export const NOT_FOUND = '/not-found';
+export const NOTFOUND = '/not-found';
 export const LOGIN = '/login';
