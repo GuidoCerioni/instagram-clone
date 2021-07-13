5ca3c11f069ed5296e7168e43bf76f0159d12231
GuidoCerioni <guidocerioni98@gmail.com>
2021-07-13 15:29:41 -0300

    add notFound component

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,4 +1,7 @@
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/header';
+import * as ROUTES from '../constants/routes';
 
 export default function NotFound() {
   useEffect(() => {
@@ -6,10 +9,26 @@ export default function NotFound() {
   }, []);
 
   return (
-    <div className="bg-gray-background">
-      <div className="max-w-screen-lg">
-        <p className="text-center text-2xl">Not found!</p>
+    <>
+      <Header />
+      <div className="bg-gray-background mx-auto px-6">
+        <div className="max-w-screen-lg">
+          <p className="text-center font-semibold text-2xl mb-4">
+            Sorry, this page isn`t available.
+          </p>
+          <p className="text-center font-normal text-md ">
+            The link you followed may be broken, or the page may have been
+            removed.
+            <Link
+              to={ROUTES.DASHBOARD}
+              className="text-blue-medium  hover:text-blue-mediumHover"
+            >
+              {' '}
+              Go back to Instagram.
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
