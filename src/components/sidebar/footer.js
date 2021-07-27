92918ef300a5baa07e8297d66c23aab8ed218743
GuidoCerioni <guidocerioni98@gmail.com>
2021-07-27 10:12:01 -0300

    add Footer

---
+++ src/components/sidebar/footer.js
@@ -0,0 +1,55 @@
+export default function Footer() {
+  return (
+    <div className="text-gray-base text-xs font-normal opacity-50 mt-4">
+      <ul className="mb-4">
+        <li className="inline-block mr-1">
+          <a
+            className=""
+            href="https://about.instagram.com/"
+            rel="nofollow noopener noreferrer"
+            target="_blank"
+          >
+            About ·
+          </a>
+        </li>
+        <li className="inline-block mr-1">
+          <a href="https://help.instagram.com/">Help ·</a>
+        </li>
+        <li className="inline-block mr-1">
+          <a href="https://about.instagram.com/blog/">Press ·</a>
+        </li>
+        <li className="inline-block mr-1">
+          <a
+            href="https://developers.facebook.com/docs/instagram"
+            rel="nofollow noopener noreferrer"
+            target="_blank"
+          >
+            API ·
+          </a>
+        </li>
+        <li className="inline-block mr-1">
+          <a href="/about/jobs/">Jobs ·</a>
+        </li>
+        <li className="inline-block mr-1">
+          <a href="/legal/privacy/">Privacy ·</a>
+        </li>
+        <li className="inline-block mr-1">
+          <a href="/legal/terms/">Terms ·</a>
+        </li>
+        <li className="inline-block mr-1">
+          <a href="/explore/locations/">Locations ·</a>
+        </li>
+        <li className="inline-block mr-1">
+          <a href="/directory/profiles/">Top Accounts ·</a>
+        </li>
+        <li className="inline-block mr-1">
+          <a href="/directory/hashtags/">Hashtags ·</a>
+        </li>
+        <li className="inline-block mr-1">
+          <span className="">Language</span>
+        </li>
+      </ul>
+      <span className="uppercase">© 2021 Instagram from Facebook</span>
+    </div>
+  );
+}
