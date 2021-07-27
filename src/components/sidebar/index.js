d3e45e931de5ef964e89e3266f30a61853d1c9d1
GuidoCerioni <guidocerioni98@gmail.com>
2021-07-27 10:12:22 -0300

    add Footer to Sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,4 +1,5 @@
 import useUser from '../../hooks/use-user';
+import Footer from './footer';
 import Suggestions from './suggestions';
 import User from './user';
 
@@ -6,13 +7,16 @@ export default function Sidebar() {
   const { user } = useUser();
   return (
     <>
-      <User username={user.username} fullName={user.fullName} />
+      <div className="mt-6 mb-4">
+        <User username={user.username} fullName={user.fullName} />
+      </div>
       <Suggestions
         userId={user.userId}
         following={user.following}
         loggedUserId={user.userId}
         loggedUserDocId={user.docId}
       />
+      <Footer />
     </>
   );
 }
