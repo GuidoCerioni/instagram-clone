bd5bb0d965ccbe6c0c9e48fe6a67bfd8d8f185a7
GuidoCerioni <guidocerioni98@gmail.com>
2021-07-27 10:13:14 -0300

    add Skeleton in Profile pictures

--- src/components/user-profile/user-photos.js
+++ src/components/user-profile/user-photos.js
@@ -1,16 +1,31 @@
 import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
 export default function UserPhotos({ photos }) {
   return (
-    // TODO: add skeleton to photos
-    <div className="grid grid-cols-3 gap-6 ">
-      {photos.map((photo, key) => (
-        <div key={key}>
-          <img src={photo.imageSrc} />
+    <div className="grid grid-cols-3 gap-2 sm:gap-6 ">
+      {!photos ? (
+        <>
+          <Skeleton width={312} height={390} />
+          <Skeleton width={312} height={390} />
+          <Skeleton width={312} height={390} />
+          <Skeleton width={312} height={390} />
+          <Skeleton width={312} height={390} />
+          <Skeleton width={312} height={390} />
+        </>
+      ) : photos.length > 0 ? (
+        photos.map((photo, key) => (
+          <div key={key}>
+            <img src={photo.imageSrc} />
+          </div>
+        ))
+      ) : (
+        <div className="flex justify-center col-span-3 text-xs phone:text-sm">
+          <p>User does not have any photos yet</p>
         </div>
-      ))}
+      )}
     </div>
   );
 }
 UserPhotos.propTypes = {
-  photos: PropTypes.array.isRequired,
+  photos: PropTypes.oneOfType([PropTypes.bool, PropTypes.array]).isRequired,
 };
