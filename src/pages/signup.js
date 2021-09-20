9fa010fb39c14f2d2e47a26a340b0ffad08be5f6
Guido Cerioni <guidocerioni98@gmail.com>
2021-09-20 20:02:26 -0300

    minor changes

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -45,7 +45,7 @@ export default function Login() {
           username: username.toLowerCase(),
           fullName,
           emailAddress: emailAddress.toLowerCase(),
-          following: ["ojmwfkQikJVM8V4pSWx1FljARKf1"],
+          following: ['ojmwfkQikJVM8V4pSWx1FljARKf1'],
           followers: [],
           dateCreated: Date.now(),
         };
